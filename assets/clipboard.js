34d9324dcb5aac944341f6197c20b543df3e9c74
bebeal <bnoahbeal@gmail.com>
2022-04-18 04:46:11 -0500

    minor updates

--- assets/clipboard.js
+++ assets/clipboard.js
@@ -1,21 +1,21 @@
-(function () {
-  function select(element) {
-    const selection = window.getSelection();
+// (function () {
+//   function select(element) {
+//     const selection = window.getSelection();
 
-    const range = document.createRange();
-    range.selectNodeContents(element);
+//     const range = document.createRange();
+//     range.selectNodeContents(element);
 
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
+//     selection.removeAllRanges();
+//     selection.addRange(range);
+//   }
 
-  document.querySelectorAll("pre code").forEach(code => {
-    code.addEventListener("click", function (event) {
-      select(code.parentElement);
+//   document.querySelectorAll("pre code").forEach(code => {
+//     code.addEventListener("click", function (event) {
+//       select(code.parentElement);
 
-      if (navigator.clipboard) {
-        navigator.clipboard.writeText(code.parentElement.textContent);
-      }
-    });
-  });
-})();
+//       if (navigator.clipboard) {
+//         navigator.clipboard.writeText(code.parentElement.textContent);
+//       }
+//     });
+//   });
+// })();
